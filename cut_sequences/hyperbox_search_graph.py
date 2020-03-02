6f078eb409ef5a74bf07442fcad9563676bafdc2
Andrea <45354085+deri96@users.noreply.github.com>
2020-03-02 15:58:27 +0100

    Update hyperbox_search_graph.py
    Co-Authored-By: robcarlucc <robcarlucc@users.noreply.github.com>

--- cut_sequences/hyperbox_search_graph.py
+++ cut_sequences/hyperbox_search_graph.py
@@ -32,51 +32,79 @@ class HyperboxSearchGraphNode:
 
 
 
-#
+# Class for defining Hyperbox's search graph
 class HyperboxSearchGraph:
 
-    #
+    # Class constructor
+    # @intervals: set of intervals that form the hyperbox
     def __init__(self, intervals):
 
+        # initializing the list of nodes
         self.nodes = list()
 
-        self.evaluated_point = None
+        # initializing evaluated node
+        self.evaluated_node = None
 
+        # defining starting node and adding it to the list of nodes
         start = HyperboxSearchGraphNode(None)
-
-        dimensions_number = len(intervals)
         self.nodes.append(list([start]))
 
+        # defining the number of dimensions of hyperbox
+        dimensions_number = len(intervals)
+
+        # for each dimension defined by the set of intervals
         for dimension_index in range(0, dimensions_number):
 
+            # define the number of intervals into the evaluated dimension
             dimension_intervals_number = len(intervals[dimension_index])
-            nodes_dim = list()
-            for dimension_interval_index in range(0, dimension_intervals_number):
-                nodes_dim.append(HyperboxSearchGraphNode(intervals[dimension_index][dimension_interval_index]))
-            self.nodes.append(nodes_dim)
 
+            # define a list of nodes for the evaluated dimension
+            nodes_from_evaluated_dimension = list()
+
+            # for each interval into the dimension, define a new node using
+            # the evaluated interval and insert it into list of nodes for the
+            # evaluated dimension
+            for interval_index in range(0, dimension_intervals_number):
+                nodes_from_evaluated_dimension.append(HyperboxSearchGraphNode(intervals[dimension_index][interval_index]))
+
+            # inserting the nodes for the evaluated dimension into the
+            # list of all nodes
+            self.nodes.append(nodes_from_evaluated_dimension)
+
+        # resetting indexes
         dimension_index = 0
-        dimension_interval_index = 1
-        while dimension_index < dimensions_number and dimension_interval_index < dimensions_number + 1:
+        interval_index = 1
+
+        # for each interval into the set of interval passed
+        while dimension_index < dimensions_number and interval_index < dimensions_number + 1:
+
+            # Structuring nodes in graph: each node is associated to a particular level n
+            # with 1 <= n <= d defining the adjacent nodes of the same node.
+            # The initial node 'start' is associated with adjacent values (which in the graph
+            # will be nodes with arcs entering from the initial node) and each subsequent node
+            # is associated with other several adjacent nodes, parts of the list 'nodes'.
+            # The nodes in the final level (i.e. those on level d) will have no adjacent values
+            # and in them the search for the hyperbox will end.
             for node in self.nodes[dimension_index]:
-                for adjacent in self.nodes[dimension_interval_index]:
+                for adjacent in self.nodes[interval_index]:
                     node.add_adjacent(adjacent)
             dimension_index = dimension_index + 1
-            dimension_interval_index = dimension_interval_index + 1
+            interval_index = interval_index + 1
 
-        self.evaluated_point = self.nodes[0][0]
-
-
-    #
-    def get_evaluated_point(self):
-        return self.evaluated_point
+        # set the evaluated node as the starting node
+        self.evaluated_node = self.nodes[0][0]
 
 
-    #
-    def set_evaluated_point(self, point):
-        self.evaluated_point = point
+    # Method for get the evaluated node
+    def get_evaluated_node(self):
+        return self.evaluated_node
 
 
-    #
-    def get_adjacent_of_evaluated_point(self):
-        return self.evaluated_point.get_adjacents()
+    # Method for set the evaluated node
+    def set_evaluated_node(self, node):
+        self.evaluated_node = node
+
+
+    # Method for get the adjacent nodes of evaluated
+    def get_adjacents(self):
+        return self.evaluated_node.get_adjacents()
