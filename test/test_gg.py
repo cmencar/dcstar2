118731957743f9742d6e1fa8a80344bc6408c452
Roberto <43984672+robcarlucci@users.noreply.github.com>
2020-04-01 17:48:17 +0200

    cleaned and fully commented code

--- test/test_gg.py
+++ test/test_gg.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import random
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 from genetic_algorithm.deap_genetic_guide import DeapGeneticGuide
 from cut_sequences.cuts_sequence import CutsSequence
 from cut_sequences.selected_cuts_sequence import SelectedCutsSequence
@@ -12,160 +12,56 @@ sys.path.append('../')
 
 # TESTING CLASS DEAPGENETICGUIDE (WITHOUT A*)
 
+# Function that generates an individual with the same number of cuts as the cuts sequence
+# @individual_class: class of the individual to create
+# @individual_dim: number of genes of the individual
 def generate(individual_class, individual_dim):
-    # definizione del genoma dell'individuo
+    # definition of individual's genome
     genome = list()
-    dimension_genome = list()
 
-    # settaggio iniziale del genoma con
-    # i geni tutti a zero
+    # initializing the genome with all genes to False
     for gene in range(individual_dim):
         genome.append(False)
 
-    # definizione di un numero casuale di geni impostati
-    # secondo la sequenza
+    # definition of a random number of genes to be modified
     random_set_genes = random.randint(2, individual_dim)
 
-    # per ogni elemento nella sequenza
-    for sequence_number in range(random_set_genes):
+    # for "random_set_genes" times
+    for i in range(random_set_genes):
 
-        # definizione di un indice casuale dove inserire
-        # il gene della sequenza
+        # definition of a random index where to change the gene
         random_index = random.randint(0, individual_dim - 1)
 
-        # se all'indice determinato esiste gia un elemento
-        # si definisce un nuovo indice
+        # if in the given index there's a gene already changed, define a new index
         while genome[random_index]:
             random_index = random.randint(0, individual_dim - 1)
 
-        # definizione del valore del gene dell'individuo
-        # all'indice valutato
+        # change the value of the evaluated gene with the defined index
         genome[random_index] = True
 
     # print("\nINDIVIDUAL GENERATED: \n", genome)
 
-    # ritorna il genoma dal quale definire l'individuo
+    # return the individual with the created genome
     return individual_class(genome)
 
-
+# Function that evaluates the ratio between the "true" genes (considered cuts) and the whole number of them
+# (considered cuts and not)
+# @individual: object that contains the genome
 def evaluate(individual):
-    # definizione della variabile di valutazione
+    # initializing evaluation variables
     valutation = 0
     total_genes = 0
-    # ogni gene True aumenta la valutazione, mentre vengono contati tutti i geni esistenti
+    # every "true" gene increments the valutation while the total number of genes is calculated
     valutation += individual.count(True)
     total_genes += len(individual)
-    # restituisce la valutazione finale
+    # returns the ratio
     return valutation / total_genes
 
-'''
-# Funzione di generazione dell'individuo. -- LISTE MULTIDIMENSIONALI
-# L'individuo sarà composto da una lista di cifre di dimensione
-# individual_dim in cui N elementi (dove N è scelto casualmente tra 1
-# e individual_dim) seguono una sequenza ordinata. Gli elementi
-# della sequenza sono posti in modo casuale all'interno della
-# lista. L'individuo, pertanto, segue la forma:
-# [False, True, False, True, True, False, True, False]
-def generate(individual_class, individual_dim):
-    # definizione del genoma dell'individuo
-    genome = list()
-    dimension_genome = list()
-
-    # settaggio iniziale del genoma con
-    # i geni tutti a zero
-    for dimension in range(len(individual_dim)):
-        dimension_genome.clear()
-        for index in range(individual_dim[dimension]):
-            dimension_genome.append(False)
-        genome.append(dimension_genome.copy())
-
-    # definizione di un numero casuale di geni impostati
-    # secondo la sequenza
-    total_genes = 0
-    for num in individual_dim:
-        total_genes += num
-    random_set_genes = random.randint(2, total_genes)
-
-    # per ogni elemento nella sequenza
-    for sequence_number in range(random_set_genes):
-
-        # definizione di un indice casuale dove inserire
-        # il gene della sequenza
-        random_dimension = random.randint(0, len(individual_dim) - 1)
-        random_index = random.randint(0, individual_dim[random_dimension] - 1)
-
-        # se all'indice determinato esiste gia un elemento
-        # si definisce un nuovo indice
-        while genome[random_dimension][random_index]:
-            random_dimension = random.randint(0, len(individual_dim) - 1)
-            random_index = random.randint(0, individual_dim[random_dimension] - 1)
-
-        # definizione del valore del gene dell'individuo
-        # all'indice valutato
-        genome[random_dimension][random_index] = True
-
-    print("\nINDIVIDUAL GENERATED: \n", genome)
-
-    # ritorna il genoma dal quale definire l'individuo
-    return individual_class(genome)
-
-# Funzione di valutazione dell'individuo -- LISTE MULTIDIMENSIONALI
-def evaluate(individual):
-    # definizione della variabile di valutazione
-    valutation = 0
-    total_genes = 0
-    # per ogni attributo dell'individuo
-    for dimension in range(len(individual)):
-        # ogni gene True aumenta la valutazione, mentre vengono contati tutti i geni esistenti
-        valutation += individual[dimension].count(True)
-        total_genes += len(individual[dimension])
-    # restituisce la valutazione finale
-    return valutation / total_genes
-'''
-'''
-def fitness(individual):
-    return (1 - evaluate(individual))*pow(pureness(individual, T_d), 5)
-'''
-'''
-# Funzione che deifince la purezza di un dato genoma -- LISTE MULTIDIMENSIONALI
-def pureness(individual, T_d, points_list, m_d=0, M_d=1):
-    # inizializzazione sequenze
-    S_d = SelectedCutsSequence()
-    S_d_b = DimensionalSequenceBinary()
-
-    # creazione della sequenza dimensionale binaria e relativa selezione di tagli dal genoma
-    S_d_b.from_binary(individual)
-    S_d.from_binary(T_d, S_d_b)
-
-    # creazione del set di hyperboxes
-    hyperboxes = S_d.generate_hyperboxes_set(points_list, m_d, M_d)
-
-    # calcolo del rapporto tra li numero di hyperbox puri e il numero complessivo di hyperbox
-    return hyperboxes.get_pure_hyperboxes_number() / hyperboxes.get_hyperboxes_number()
-'''
-
-# Inizializzatore dei numeri casuali
+# initialize the seed for random numbers
 random.seed()
 
-'''
-# definizione di prototipi
-print("\n----- prototypes -----\n")
-points = [
-    Point(coordinates=[.2354, .34, .543], label="prototype_1", name="point_A"),
-    Point(coordinates=[.3345, .3421, .36897], label="prototype_1", name="point_B"),
-    Point(coordinates=[.351, .3453, .6423], label="prototype_2", name="point_C"),
-    Point(coordinates=[.45235, .00009, .141], label="prototype_1", name="point_D"),
-    Point(coordinates=[.9, .5444, .25434], label="prototype_1", name="point_E"),
-    Point(coordinates=[.999, .4, .7714], label="prototype_2", name="point_F"),
-    Point(coordinates=[.799, .24, .1114], label="prototype_1", name="point_G")
-]
-print(points)
-# definizione di un T_d
-print("\n----- T_d -----\n")
-T_d = CutsSequence([[.2, .36, .56, .63, .87, .88, .94], [.11, .32, .34, .36, .712, .998], [.02, .47, 0.7111, .89]])
-T_d.debug_print()
-'''
-points1 = [
+# define of example prototypes and cuts
+points_example = [
     Point(coordinates=[.2354, .34], label="prototype_1", name="point_A"),
     Point(coordinates=[.3345, .3421], label="prototype_1", name="point_B"),
     Point(coordinates=[.351, .3453], label="prototype_2", name="point_C"),
@@ -174,107 +70,106 @@ points1 = [
     Point(coordinates=[.999, .4], label="prototype_2", name="point_F"),
     Point(coordinates=[.799, .24], label="prototype_1", name="point_G")
 ]
-T_d_1 = CutsSequence([[.28495, .34275, .40225, .625675, .8495, .9495], [.120045, .29, .34105, .3437, .37265, .4722]])
+T_d_example = CutsSequence([[.28495, .34275, .40225, .625675, .8495, .9495], [.120045, .29, .34105, .3437, .37265, .4722]])
 
-# Memorizzazione grandezza dimensioni T_d
+# calculate the size of each dimension of cuts sequence
 genes_per_dimension = list()
-for dimension in range(T_d_1.get_dimensions_number()):
-    genes_per_dimension.append(len(T_d_1.get_dimension(dimension)))
+for dimension in range(T_d_example.get_dimensions_number()):
+    genes_per_dimension.append(len(T_d_example.get_dimension(dimension)))
 
-# Numero totale di geni dell'individuo
+# calculate the total number of genes that the genome will have
 genes_number = 0
 for num in genes_per_dimension:
     genes_number += num
 
-# Numero di individui selezionati per la
-# selezione per torneo
+# set number of individuals for tournament selection
 selected_for_tournament = 5
 
-# Numero di individui
-# N.B. Per evitare la convergenza è utile inserire
-# la grandezza della popolazione maggiore del
-# numero di generazioni
+# calculate population
+# N.B.: population is given by duplicating the number of genes in the genome
 population_size = 2 * genes_number
 
-# Generazioni di evoluzioni della popolazione
+# set number of generations
 generations = 20
 
-# grado di mutazione (percentuale)
+# calculate mutation rate
+# N.B.: mutation rate is calculated by reciprocating the number of genes
 mutation_rate = 1 / genes_number
 
-# grado di accoppiamento (percentuale)
+# set mating rate
 mating_rate = 0.7
 
-# numero di individui finali acquisiti
+# set number of best individuals to retrieve
 selected_best = 10
 
-# thing = generate(list, individual_elements_per_dimension) -- TEST DI "GENERATE"
-# evaluation_thing = evaluate(thing) -- TEST DI "EVALUATE"
-
-# definizione di un oggetto per la creazione della guida genetica con liste multidimensionali
-# genetic_guide = DeapGeneticGuide(evaluate, generate, individual_elements_per_dimension, mutation_rate, mating_rate,
-#                                 selected_for_tournament, T_d, points)
-
-# definizione di un oggetto per la creazione della guida genetica con liste monolitiche
+# define DGG object to create the genetic guide with monodimensional lists
 genetic_guide = DeapGeneticGuide(evaluate, generate, genes_number, mutation_rate, mating_rate, selected_for_tournament,
-                                 T_d_1, points1, genes_per_dimension)
+                                 T_d_example, points_example, genes_per_dimension)
 
-# evoluzione e acquisizione degli individui migliori dalla guida genetica con liste multidimensionali
-# best_individuals = genetic_guide.evolve(population_size, generations, selected_best, T_d,
-#                                        individual_elements_per_dimension)
+# evolution and acquisition of best individuals from genetic guide with monodimensional lists
+best_individuals = genetic_guide.evolve(population_size, generations, selected_best)
 
-# evoluzione e acquisizione degli individui migliori dalla guida genetica con liste monolitiche
-best_individuals = genetic_guide.evolve(population_size, generations, selected_best, T_d_1,
-                                        genes_per_dimension)
-
-# riconversione da lista monolitica a "sequenza di tagli dimensionali"
+# convert from monodimensional list to binary cuts sequence
+# initialize list of sequences (multidimensional individuals) and other support lists
 list_of_sequences = list()
 sequence = list()
 dimension = list()
+# for each individual in the best's list
 for individual in best_individuals:
+    # initialize index, offset for keeping track of the dimensions to create and clear support sequence list
     sequence.clear()
     offset = 0
     i = 0
+    # for each number of genes of the dimension that is going to be created
     for num_elem in genes_per_dimension:
+        # clear "dimension" support list
         dimension.clear()
+        # increment offset by the number of elements that are going into the evaluated dimension
         offset = offset + num_elem
+        # while there are genes
         while i < offset:
+            # append into "dimension" support list the evaluated gene and increment index
             dimension.append(individual[i])
             i += 1
+        # append a copy of created "dimension" into "sequence" support list
         sequence.append(dimension.copy())
+    # append a copy of created "sequence" into the list of sequences
     list_of_sequences.append(sequence.copy())
 
+# create selected cuts sequence and binary dimensional sequence objects
 S_d = SelectedCutsSequence()
 S_d_b = DimensionalSequenceBinary()
 
-# stampa a video di ogni individuo
+# show every "pure" individual or a "not found" message
 print("\n---------------------------------------------------------")
+# set flag to "pure individual not found"
 no_pure = True
+# for each individual in the list of sequences
 for individual in list_of_sequences:
+    # generate binary sequence from the individual
     S_d_b.from_binary(individual)
-    S_d.from_binary(T_d_1, S_d_b)
-    hyperboxes = S_d.generate_hyperboxes_set(points1, 0, 1)
+    # generate selected cuts sequence from cuts sequence and newly generated binary sequence
+    S_d.from_binary(T_d_example, S_d_b)
+    # create set of hyperboxes from selected cuts sequence, points list, m_d and M_d
+    hyperboxes = S_d.generate_hyperboxes_set(points_example, 0, 1)
+    # if all of the hyperboxes generated are pure
     if hyperboxes.get_impure_hyperboxes_number() == 0:
+        # print the pure individual and set flag to False
         print("\nPURE INDIVIDUAL: \n", individual)
-
         no_pure = False
-
         '''
         for cut in S_d.get_dimension(0):
             plt.plot([cut, cut], [0, 1], 'k', linestyle='--', color='black')
-
         for cut in S_d.get_dimension(1):
             plt.plot([0, 1], [cut, cut], linestyle='--', color='black')
-
         for point in points1:
             if point.get_label() == "prototype_1":
                 color = 'ro'
             elif point.get_label() == "prototype_2":
                 color = 'bo'
-
             plt.plot(point.get_coordinate(0), point.get_coordinate(1), color)
-
         plt.show()
         '''
+# if none of the individuals generated has all pure hyperboxes show a message
 if no_pure:
-     print("\nNONE OF THE GENERATED INDIVIDUALS HAS PURE HYPERBOXES")
+    print("\nNONE OF THE GENERATED INDIVIDUALS HAS PURE HYPERBOXES")
