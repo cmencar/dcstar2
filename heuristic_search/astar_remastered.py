0bd353eab0066eb7b396041e8523d1e18ed8d9e7
Andrea <45354085+deri96@users.noreply.github.com>
2020-04-21 09:51:00 +0200

    Update astar_remastered.py

--- heuristic_search/astar_remastered.py
+++ heuristic_search/astar_remastered.py
@@ -19,10 +19,7 @@ def astar(problem):
     front = PriorityQueue()
 
     start_node = Node(problem.start_state)
-    #start_node = DCStarNode(problem.start_state)
-
-    #estimated_node = (problem.estimate_cost(start_node.path()), start_node)
-    #estimated_node = (problem.estimate_cost(start_node), start_node)
+    
     estimated_node = ((0, 0, 0), start_node)
 
     front.put(estimated_node)
@@ -38,18 +35,14 @@ def astar(problem):
         else:
             sys.stdout.write('\r' + "Evaluating" + str('.' * (branches_taken % 25)))
 
-        #(path_estimated_cost, current_node) = front.get()
         (estimated_cost, current_node) = front.get()
 
         current_state = current_node.state
 
-        #if not problem.unique_successors:
         if closed.find(current_state) is None:
-            #closed.put((path_estimated_cost, current_node))
             closed.put((estimated_cost, current_node))
 
         if problem.goal(current_state):
-            #return (current_node.path(), branches_taken)
             return current_node.state, branches_taken, time.time() - start_time
 
         else:
@@ -60,22 +53,18 @@ def astar(problem):
 
                 successor_node = Node(successor_state, parent_node = current_node)
 
-                #path_estimated_cost = problem.estimate_cost(successor_node.path())
                 estimated_cost = problem.estimate_cost(successor_node)
 
-                #if not problem.unique_successors:
                 if closed.find(current_state) is not None:
 
                     estimated_node = front.find(successor_state)
 
                     if estimated_node is not None:
 
-                        #if path_estimated_cost < estimated_node[0]:
                         if estimated_cost < estimated_node[0]:
 
                             front.remove(estimated_node)
 
-                            #front.put((path_estimated_cost, successor_node))
                             front.put((estimated_cost, successor_node))
 
                     else:
@@ -84,20 +73,16 @@ def astar(problem):
 
                         if estimated_node is not None:
 
-                            #if path_estimated_cost < estimated_node[0]:
                             if estimated_cost < estimated_node[0]:
 
-                                #front.put((path_estimated_cost, successor_node))
                                 front.put((estimated_cost, successor_node))
 
                                 closed.remove(estimated_node)
 
                         else:
-                            #front.put((path_estimated_cost, successor_node))
                             front.put((estimated_cost, successor_node))
 
                 else:
-                    #front.put((path_estimated_cost, successor_node))
                     front.put((estimated_cost, successor_node))
 
     return None, branches_taken, time.time() - start_time
